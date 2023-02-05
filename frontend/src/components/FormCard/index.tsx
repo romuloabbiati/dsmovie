6ed70218ede058413b156a332ac5229eb141c202
romuloabbiati <rhabbiati@hotmail.com>
2023-02-05 06:33:32 +0000

    show score

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Movie } from 'types/movie';
 import { BASE_URL } from 'utils/requests';
 import './styles.css';
